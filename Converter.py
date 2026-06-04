9f5762855f974b300bee6fef365deeb62935defc
Semen Haydovich <vlasovdl.id@gmail.com>
2026-06-04 18:39:06 +0300

    2

--- Converter.py
+++ Converter.py
@@ -3,16 +3,18 @@ class DefaultNode:
     type=''
     spec=''
     children = []
-    def __init__(self):
-        pass
+    available=[]
 
-    
+    def __init__(self):
+        self.children = []
+
+
+    def check_children(self):
+        pass
 
 class Sequence(DefaultNode):
     type = 'Sequence'
     approve=['Unl','Operation']
-
-
 
 class OperationIf(DefaultNode):
     type = 'Operation'
@@ -33,6 +35,31 @@ class Term(DefaultNode):
 
 class Factor(DefaultNode):
     type = 'Factor'
-    approve=['Opt',]
+    approve = ['One','Variable','Constant','Math','One_End']
+
+class Variable(DefaultNode):
+    type = 'Variable'
+    available = [int,bool]
+    def __init__(self,data):
+        super().__init__()
+        self.data=data
+
+class Constant(DefaultNode):
+    type = 'Constant'
+    available = [int,bool]
+    def __init__(self,data):
+        super().__init__()
+        self.data=data
+
+class Return(DefaultNode):
+    type = 'Operation'
+    spec='Return'
+    approve = ["Factor"]
+
+class Assignment(DefaultNode):
+    type = 'Operation'
+    spec='Assignment'
+    approve = ["Variable",'Math']
+
 
 
