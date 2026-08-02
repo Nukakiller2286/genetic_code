94ae21c3eaca8f42a2eddd831aa3c469a1c178b8
Nukakiller2286 <vlasovdl.id@gmail.com>
2026-08-02 15:51:58 +0500

    check

--- Node.cpp
+++ Node.cpp
@@ -3,3 +3,70 @@
 //
 
 #include "Node.h"
+
+bool Node::check() {
+    std::vector<std::unique_ptr<Slot>> approved=get_approved(type,spec);
+    unsigned int child_i=0;
+    unsigned int app_i=0;
+
+    while (app_i<approved.size()) {
+        switch (approved[app_i]->quantifier) {
+            case Quantifier::ONE: {
+                if (not(children[child_i]->spec==approved[app_i]->spec && children[child_i]->type==approved[app_i]->type)){
+                    return false;}
+                ++child_i;
+                break;
+            case Quantifier::ONE_OPT:
+            ++child_i;
+            break;
+            }
+            case Quantifier::UNL:{
+                unsigned int c=0;
+                while (children[child_i]->spec==approved[app_i]->spec && children[child_i]->type==approved[app_i]->type) {
+                    ++child_i;
+                    ++c;
+                    }
+                if (c==0) {
+                    return false;
+                }
+                break;
+            }
+            case Quantifier::UNL_OPT: {
+                while (children[child_i]->spec==approved[app_i]->spec && children[child_i]->type==approved[app_i]->type) {
+                    ++child_i;
+                }
+                break;
+            }
+            case Quantifier::UNL_OPT_START: {
+                unsigned int size_app_i;
+                for (size_app_i=1 ; app_i+size_app_i<approved.size(); ++size_app_i) {
+                    if (approved[app_i+size_app_i]->quantifier==Quantifier::UNL_OPT_END) {
+                        break;
+                    }
+                }
+                unsigned int end_app_i=app_i+size_app_i;
+                child_i+=size_app_i;
+                size_app_i+=1;
+                while (children[child_i]->spec==approved[end_app_i]->spec && children[child_i]->type==approved[end_app_i]->type) {
+                    for (int i = 0; i <= size_app_i; ++i) {
+                        if (not(children[child_i-i]->spec==approved[end_app_i-i]->spec && children[child_i-i]->type==approved[end_app_i-i]->type)){
+                            return false;}
+                    }
+                    child_i+=size_app_i;
+                }
+
+                break;
+            }
+
+
+            ++app_i;
+
+        }
+    }
+    if (children.empty()) {
+        return true;
+    }
+    for (const auto& chld : children) {
+        return chld->check();
+    }
+}
--- Node.h
+++ Node.h
@@ -4,45 +4,128 @@
 
 #ifndef UNTITLED_DEFAULTNODE_H
 #define UNTITLED_DEFAULTNODE_H
+#include <iostream>
 #include <memory>
 #include <string>
 #include <vector>
 
 enum class OpType {
-    // арифметика
-    ADD, SUB, MUL, DIV,
-    // сравнения
-    GT, LT, EQ,
-    // логика
-    AND, OR, NOT,
-    // структуры
-    IF, SEQ, ASSIGN, RETURN,
-    // листья
-    VAR, CONST
+    SEQ, OPER,
+    COND, MATH, TERM, FACTOR,
+    FLT,
+    ADD, MULT, COMP
+};
+
+enum class Spec {
+    IF, WHILE, RETURN, ASSIGN, NONE, VAR, CONST
 };
 
 enum class Quantifier {
     ONE,
     OPT,
     UNL,
-    UNL_OPT
+    UNL_OPT,
+    UNL_OPT_START,
+    UNL_OPT_END,
+    ONE_OPT
 };
 struct Slot {
-    OpType op;
-    Quantifier q = Quantifier::ONE;
+    OpType type;
+    Quantifier quantifier = Quantifier::ONE;
+    Spec spec = Spec::NONE;
 };
 
-std::vector<Slot *> get_approved(OpType op) {
-    s
+inline std::vector<std::unique_ptr<Slot>> get_approved(OpType op, Spec s = Spec::NONE) {
+    std::vector<std::unique_ptr<Slot>> v;
+    switch (op) {
+        case OpType::SEQ: {
+
+            v.push_back(std::make_unique<Slot>(Slot{OpType::OPER, Quantifier::UNL_OPT}));
+            return v;
+        }
+        case OpType::OPER:
+        {
+            switch (s) {
+                case Spec::ASSIGN: {
+
+                        v.push_back(std::make_unique<Slot>(Slot{OpType::FLT, Quantifier::ONE, Spec::VAR}));
+                        v.push_back(std::make_unique<Slot>(Slot{OpType::MATH, Quantifier::ONE}));
+                        return v;
+                }
+                    case Spec::IF: {
+
+                        v.push_back(std::make_unique<Slot>(Slot{OpType::COND, Quantifier::ONE}));
+                        v.push_back(std::make_unique<Slot>(Slot{OpType::SEQ, Quantifier::ONE}));
+                        v.push_back(std::make_unique<Slot>(Slot{OpType::OPER, Quantifier::UNL_OPT, Spec::IF}));
+                        v.push_back(std::make_unique<Slot>(Slot{OpType::SEQ, Quantifier::ONE_OPT}));
+                        return v;
+                }
+                    case Spec::WHILE: {
+
+                        v.push_back(std::make_unique<Slot>(Slot{OpType::COND, Quantifier::ONE}));
+                        v.push_back(std::make_unique<Slot>(Slot{OpType::SEQ, Quantifier::ONE}));
+                        return v;
+                }
+                    case Spec::RETURN: {
+
+                        v.push_back(std::make_unique<Slot>(Slot{OpType::FLT, Quantifier::UNL, Spec::VAR}));
+                        return v;
+                }
+
+
+                default:std::cerr << "ERROR: Unknown specification type." << std::endl; ;
+            }
+        }
+        case OpType::COND:
+        {
+
+            v.push_back(std::make_unique<Slot>(Slot{OpType::MATH, Quantifier::ONE}));
+            v.push_back(std::make_unique<Slot>(Slot{OpType::COND, Quantifier::ONE}));
+            v.push_back(std::make_unique<Slot>(Slot{OpType::MATH, Quantifier::ONE}));
+            return v;
+        }
+        case OpType::MATH:
+        {
+
+            v.push_back(std::make_unique<Slot>(Slot{OpType::TERM, Quantifier::ONE}));
+            v.push_back(std::make_unique<Slot>(Slot{OpType::ADD, Quantifier::UNL_OPT_START}));
+            v.push_back(std::make_unique<Slot>(Slot{OpType::TERM, Quantifier::UNL_OPT_END}));
+            return v;
+        }
+        case OpType::TERM:
+        {
+
+            v.push_back(std::make_unique<Slot>(Slot{OpType::FACTOR, Quantifier::ONE}));
+            v.push_back(std::make_unique<Slot>(Slot{OpType::MULT, Quantifier::UNL_OPT_START}));
+            v.push_back(std::make_unique<Slot>(Slot{OpType::FACTOR, Quantifier::UNL_OPT_END}));
+            return v;
+        }
+        case OpType::FACTOR:
+        {
+
+            v.push_back(std::make_unique<Slot>(Slot{OpType::FLT, Quantifier::ONE_OPT}));
+            v.push_back(std::make_unique<Slot>(Slot{OpType::MATH, Quantifier::ONE_OPT}));
+            return v;
+        }
+            default: {
+            std::cerr << "ERROR: Unknown operation type." << std::endl;
+        }
+        return v;
+    }
+
 
 };
 
 class Node {
 public:
     Node();
-
+    std::vector<float>  evaluate(std::vector<float> input);
+    bool check();
+    
+    OpType type;
+    Spec spec;
     std::vector<std::unique_ptr<Node>> children;
-    float data;
+    std::string name;
 
 };
 
